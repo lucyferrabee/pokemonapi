825267e3f7de584bedeed1e62b30eaaa3a8ee425
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-06-24 13:35:36 +0100

    fixed the foreach for the fetch to grab the pokemon names

--- app/js/app.ts
+++ app/js/app.ts
@@ -1,7 +1,10 @@
 
-
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=9').then(response => {
-    return response.json()
-}).then(data => {
-    document.querySelector('#name').textContent += data;
+fetch('https://pokeapi.co/api/v2/pokemon/?limit=9')
+    .then(response => {
+        return response.json()
+    }).then(data => {
+    (data.results).forEach(entry => {
+        document.querySelector('#name').innerHTML += (entry.name + '<br>');
+        console.log(entry.name)
+    })
 })
